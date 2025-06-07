00ef8d0559bb6e384c55cf0d0137a5b54d6e480f
Favour@Afripoint <favour.ezeh@afripointgroup.com>
2025-06-07 10:02:50 +0100

    purchase

--- src/App.tsx
+++ src/App.tsx
@@ -262,7 +262,7 @@ const AppContent: React.FC = () => {
           <Route path="/payment-method" element={<PaymentMethodPage />} />
           <Route path="/certificate" element={<Certificate />} />
           <Route path="/verify-certificate" element={<VerifyCertificatePage />} />
-          <Route path="/successpayment" element={<VerifyPayment/>} />
+          <Route path="/verifypayment" element={<VerifyPayment/>} />
           {/*<Route path="/paymentcallback" element={<PaymentCallback/>} />
           <Route path="/success" element={<SuccessPaymentPage/>} />  */}
 
